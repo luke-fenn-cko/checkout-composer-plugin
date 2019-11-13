e70700ca29363853d390badb3a33b77fa8af1dab
Luke.Fenn <luke.fenn@CKO-3184.local>
2019-11-13 12:09:26 +0000

    For testing: should define custom composer command and print to console when ran

---
+++ src/Controllers/ConsoleController.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Checkout.com
+ * Authorised and regulated as an electronic money institution
+ * by the UK Financial Conduct Authority (FCA) under number 900816.
+ *
+ * PHP version 7
+ *
+ * @category  SDK
+ * @package   Checkout.com
+ * @author    Platforms Development Team <platforms@checkout.com>
+ * @copyright 2010-2019 Checkout.com
+ * @license   https://opensource.org/licenses/mit-license.html MIT License
+ * @link      https://docs.checkout.com/
+ */
+
+namespace Checkout\Controllers;
+
+class ConsoleController {
+
+    /**
+     * @var MageVersion
+     */
+    public $mageVersion;
+
+          /**
+     * @var ModuleVersion
+     */
+    public $moduleVersion;
+
+
+    public function logStatus() {
+        printf( 'Hello World');
+    }
+
+//    public function warnPermissions() {
+//
+//    }
+
+}
